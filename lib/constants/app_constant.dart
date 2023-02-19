5a044cfdede8e621030e2ee77535207e75db4fef
Thakshila <thakshila@optimosoftware.co.uk>
2023-02-19 22:57:18 +0530

    ios settings

--- lib/constants/app_constant.dart
+++ lib/constants/app_constant.dart
@@ -1,10 +1,7 @@
-class AppConstant{
-  
+class AppConstant {
   static const String appName = "LMS";
   static const String appVersion = "1.0";
-  static const String baseUrl ="https://www.straniericattolici.it/thakshila/lms/";// "http://192.168.1.100:8080/lms/";
+  static const String baseUrl =
+      "https://www.straniericattolici.it/thakshila/lms/"; // "http://192.168.1.100:8080/lms/";
   static const int apiTimeout = 60;
-  
 }
-
-
